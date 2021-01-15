417dfdee8a8ae4201fa21b7fa1e1e91a16f9178d
CetinMehmet <cetin43mehmet@gmail.com>
2021-01-15 16:04:27 +0100

    New script added to produce violin plots

--- generate_violinplots.py
+++ generate_violinplots.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
 import pandas as pd
-
+import seaborn as sns
 
 SHOW_PLOT = False
 DAS_PATH = "/var/scratch/lvs215/processed-surf-dataset/"
 SAVEFIG_PATH= "/home/cmt2002/rack-barplots/"
-FIGNAME = "rack_barplots"
+FIGNAME = "rack_violinplots"
 
 def get_rack_nodes(df):
     rack_nodes = {}
@@ -32,78 +32,52 @@ def get_custom_values(df):
 
     return values
 
-def load1_rack_violinplot(ax, df_covid, df_non_covid, subtitle):
-     rack_nodes = self.__get_rack_nodes(df_covid) # To get the rack nodes
-        rack_values = list()
-        rack_names = list()
-        violin_width = 0.8
-        
-        for rack, columns in rack_nodes.items():
-            arr_covid = self.__get_custom_values(df_covid[list(columns)])
-            arr_non_covid = self.__get_custom_values(df_non_covid[list(columns)])
-            rack_values.append(arr_covid)
-            rack_values.append(arr_non_covid)
-            rack_names.append(rack)
-            
-        sns.violinplot(data=rack_values, ax=ax, cut=0, width=violin_width, palette=['lightcoral', 'steelblue'] * (int(len(rack_values)/2)))
-        ax.set_ylabel(self.ylabel, fontsize=32)
-        #ax.set_ylim(0, 100)
-        ax.set_ylim(0, )
-        ax.tick_params(axis='both', which='major', labelsize=32)
-        ax.tick_params(axis='both', which='minor', labelsize=32)
-        ax.set_xticks([i + 0.5 for i in range(0, len(rack_values), 2)])
-        ax.set_xlabel(subtitle, fontsize=30)
-
-        # Depcit the values that exceed 100 load
-        #for index, val in enumerate(rack_values):
-            #max_val = np.amax(val)
-            #if max_val > 100:
-                #ax.text(x=index-0.2, y=102.2, s=str(int(max_val)), fontsize=22, color="black", va="center")
-
-        ax.set_xticklabels(
-            rack_names,
-            ha='center', fontsize=32
-        )
-        for i in range(0, len(rack_values), 2):
-            ax.axvline(i + 1.5, lw=2, ls='dashed')
 
 def rack_violinplot(ax, df_covid, df_non_covid, subtitle, ylabel):
-    rack_nodes = get_rack_nodes(df_covid)  # Get the rack nodes
-    index = 0
-    w = 0.4
+    rack_nodes = get_rack_nodes(df_covid) # To get the rack nodes
+    rack_values = list()
+    rack_names = list()
+    violin_width = 0.8
+    
     for rack, columns in rack_nodes.items():
         arr_covid = get_custom_values(df_covid[list(columns)])
         arr_non_covid = get_custom_values(df_non_covid[list(columns)])
+        rack_values.append(arr_covid)
+        rack_values.append(arr_non_covid)
+        rack_names.append(rack)
+        
+    sns.violinplot(data=rack_values, ax=ax, cut=0, width=violin_width, palette=['lightcoral', 'steelblue'] * (int(len(rack_values)/2)))
+    ax.set_ylabel(ylabel,fontsize=14)
+    ax.tick_params(axis='both', which='major', labelsize=14)
+    ax.tick_params(axis='both', which='minor', labelsize=14)
+    ax.set_xticks([i + 0.5 for i in range(0, len(rack_values), 2)])
+    ax.set_xlabel(subtitle, fontsize=14)
 
-        ax.bar(x=index - w / 2, height=arr_covid.mean(), width=w, yerr=arr_covid.std(), color="lightcoral",
-                     capsize=4)
-        ax.bar(x=index + w / 2, height=arr_non_covid.mean(), width=w, yerr=arr_non_covid.std(), color="steelblue",
-                     capsize=4)
-        index += 1
+    # For load1, depict the values that exceed 100 load
+    if ylabel == "Load1":
+        ax.set_ylim(0, 100)
+        for index, val in enumerate(rack_values):
+            max_val = np.amax(val)
+            if max_val > 100:
+                ax.text(x=index-0.2, y=102.2, s=str(int(max_val)), fontsize=22, color="black", va="center")
 
-    ax.tick_params(axis='both', which='major', labelsize=16)
-    ax.tick_params(axis='both', which='minor', labelsize=16)
-    ax.set_ylabel(ylabel, fontsize=14)
-    ax.set_ylim(0, )
-    ax.set_xlabel(subtitle, fontsize=16)
-    ax.set_xticks(np.arange(len(rack_nodes.keys())))
-    ax.set_xticklabels(rack_nodes.keys(), fontsize=16)
+    else:
+        ax.set_ylim(0, )
+
+    ax.set_xticklabels(
+        rack_names,
+        ha='center', fontsize=16
+    )
+    for i in range(0, len(rack_values), 2):
+        ax.axvline(i + 1.5, lw=2, ls='dashed')
 
 def rack_analysis_violinplot(df_dic, ax, ylabel):
-    rack_barplot(
+    rack_violinplot(
         ax=ax,
         df_covid=df_dic["covid"],
         df_non_covid=df_dic["non_covid"],
         subtitle=None,
         ylabel=ylabel)
-    ax.axvline(x=9.5, c="green", lw=1.5)
-
-def load_rack_analysis_violinplot(df_dic, ax):
-    load1_rack_barplot(
-        ax=ax,
-        df_covid=df_dic["covid"],
-        df_non_covid=df_dic["non_covid"],
-        subtitle=None)
     ax.axvline(x=9.5, c="green", lw=1.5)
 
 def covid_non_covid(df):
@@ -125,18 +99,18 @@ df_temp_covid, df_temp_non_covid = covid_non_covid(pd.read_parquet(DAS_PATH + "s
 
 
 _, (ax_power, ax_load, ax_temp, ax_ram) = plt.subplots(4, 1, figsize=(11, 8), constrained_layout=True, sharex=True)
-rack_analysis_barplot(
+rack_analysis_violinplot(
     df_dic={"covid": df_power_covid, "non_covid": df_power_non_covid},
     ax=ax_power,
     ylabel="Power consumption [W]")
-rack_analysis_barplot(
+rack_analysis_violinplot(
     df_dic={"covid": df_temp_covid, "non_covid": df_temp_non_covid},
     ax=ax_temp,
     ylabel="Temperature [C]")
-load_rack_analysis_barplot(
+rack_analysis_violinplot(
     df_dic={"covid": df_load_covid, "non_covid": df_load_non_covid},
     ax=ax_load)
-rack_analysis_barplot(
+rack_analysis_violinplot(
     df_dic={"covid": df_ram_covid, "non_covid": df_ram_non_covid},
     ax=ax_ram,
     ylabel="RAM utilization [%]")
